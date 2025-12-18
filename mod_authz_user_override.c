31ece3ae393812e159004bffcba304c31bb374af
carrvo <carrvo@gmail.com>
2025-12-17 20:25:46 -0700

    first round strip away

--- mod_authz_user_override.c
+++ mod_authz_user_override.c
@@ -29,21 +29,21 @@
 
 typedef struct {
         int dummy;  /* just here to stop compiler warnings for now. */
-} authz_user_config_rec;
+} authz_user_override_config_rec;
 
-static void *create_authz_user_dir_config(apr_pool_t *p, char *d)
+static void *create_authz_user_override_dir_config(apr_pool_t *p, char *d)
 {
-    authz_user_config_rec *conf = apr_palloc(p, sizeof(*conf));
+    authz_user_override_config_rec *conf = apr_palloc(p, sizeof(*conf));
 
     return conf;
 }
 
-static const command_rec authz_user_cmds[] =
+static const command_rec authz_user_override_cmds[] =
 {
     {NULL}
 };
 
-module AP_MODULE_DECLARE_DATA authz_user_module;
+module AP_MODULE_DECLARE_DATA authz_user_override_module;
 
 static authz_status user_check_authorization(request_rec *r,
                                              const char *require_args,
@@ -62,7 +62,7 @@ static authz_status user_check_authorization(request_rec *r,
     require = ap_expr_str_exec(r, expr, &err);
     if (err) {
         ap_log_rerror(APLOG_MARK, APLOG_ERR, 0, r, APLOGNO(02594)
-                      "authz_user authorize: require user: Can't "
+                      "authz_user_override authorize: require user: Can't "
                       "evaluate require expression: %s", err);
         return AUTHZ_DENIED;
     }
@@ -80,17 +80,6 @@ static authz_status user_check_authorization(request_rec *r,
                   r->uri, r->user);
 
     return AUTHZ_DENIED;
-}
-
-static authz_status validuser_check_authorization(request_rec *r,
-                                                  const char *require_line,
-                                                  const void *parsed_require_line)
-{
-    if (!r->user) {
-        return AUTHZ_DENIED_NO_USER;
-    }
-
-    return AUTHZ_GRANTED;
 }
 
 static const char *user_parse_config(cmd_parms *cmd, const char *require_line,
@@ -112,35 +101,26 @@ static const char *user_parse_config(cmd_parms *cmd, const char *require_line,
     return NULL;
 }
 
-static const authz_provider authz_user_provider =
+static const authz_provider authz_user_override_provider =
 {
     &user_check_authorization,
     &user_parse_config,
 };
-static const authz_provider authz_validuser_provider =
-{
-    &validuser_check_authorization,
-    NULL,
-};
 
 static void register_hooks(apr_pool_t *p)
 {
-    ap_register_auth_provider(p, AUTHZ_PROVIDER_GROUP, "user",
+    ap_register_auth_provider(p, AUTHZ_PROVIDER_GROUP, "user-override",
                               AUTHZ_PROVIDER_VERSION,
-                              &authz_user_provider, AP_AUTH_INTERNAL_PER_CONF);
-    ap_register_auth_provider(p, AUTHZ_PROVIDER_GROUP, "valid-user",
-                              AUTHZ_PROVIDER_VERSION,
-                              &authz_validuser_provider,
-                              AP_AUTH_INTERNAL_PER_CONF);
+                              &authz_user_override_provider, AP_AUTH_INTERNAL_PER_CONF);
 }
 
-AP_DECLARE_MODULE(authz_user) =
+AP_DECLARE_MODULE(authz_user_override) =
 {
     STANDARD20_MODULE_STUFF,
-    create_authz_user_dir_config, /* dir config creater */
+    create_authz_user_override_dir_config, /* dir config creater */
     NULL,                         /* dir merger --- default is to override */
     NULL,                         /* server config */
     NULL,                         /* merge server config */
-    authz_user_cmds,              /* command apr_table_t */
+    authz_user_override_cmds,              /* command apr_table_t */
     register_hooks                /* register hooks */
 };
